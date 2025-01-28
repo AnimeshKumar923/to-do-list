609724028914d292d8c8c67a78469835af324e56
Animesh Kumar <kumaranimesh923@gmail.com>
2025-01-29 00:00:45 +0530

    fix: add new task handler button

--- src/modal.js
+++ src/modal.js
@@ -1,21 +1,28 @@
 export default function(){
   const modal = document.querySelector('.modal');
-  const btn = document.querySelector('.card-button');
+  const infoBtn = document.querySelector('.card-button');
+  const close = document.querySelector('.close-modal');
+  const addTaskBtn = document.querySelector('.add-task');
 
   // When the user clicks on the button, open the modal
-  btn.addEventListener('click',  () => {
-    modal.showModal();
+  infoBtn.addEventListener('click',  () => {
+    // show a read-only view of the added information
+    // modal.showModal();
   })
-
-  // When the user clicks on <span> (x), close the modal
-  // span.onclick = function() {
-  //   modal.style.display = "none";
-  // }
-
+  
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
   }
+  
+  close.addEventListener('click', () => {
+    modal.close();
+  })
+  
+  addTaskBtn.addEventListener('click', () => {
+    modal.showModal();
+    
+  })
 };
